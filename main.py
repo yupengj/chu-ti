8ba788dac4f2e748b14fd6a0146fa83440a4d262
jyp1 <jyp10@qq.com>
2022-11-20 16:20:06 +0800

    v2

--- main.py
+++ main.py
@@ -1,15 +1,137 @@
-# 这是一个示例 Python 脚本。
+from tkinter import *
+from tkinter import ttk
+from tkhtmlview import HTMLText
+import win32api
+import win32print
+import tempfile
+import pdfkit
+import html_template
+import question_bank
+import subprocess
 
-# 按 Shift+F10 执行或将其替换为您的代码。
-# 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
-import suanshu
+print_html = []
 
-# 按间距中的绿色按钮以运行脚本。
-if __name__ == '__main__':
-    tis = suanshu.chu_ti(100, 200, 3, 10)
-    for x in tis:
-        print(x)
+max_width = 1300
+max_height = 1000
 
-# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
+root = Tk()
+root.title("自动出题")
+root.geometry("{}x{}".format(max_width, max_height))
 
 
+# 获取打印机列表
+def get_printer_list():
+    print_list = []
+    printers = list(win32print.EnumPrinters(2))
+    for i in printers:
+        print_list.append(i[2])
+    print(print_list)
+    return print_list
+
+
+# 增加选择打印机组件
+printerLab = Label(root, text="选择打印机")
+printerValue = StringVar()
+printerEntity = ttk.Combobox(root, textvariable=printerValue)
+printerEntity['values'] = get_printer_list()
+printerValue.set("Canon TS3300 series")
+printerLab.place(x=0, y=10, width=100, anchor='nw')
+printerEntity.place(x=90, y=10, width=200, anchor='nw')
+
+
+def html_to_pdf(html_file):
+    # Define path to wkhtmltopdf.exe
+    path_to_wkhtmltopdf = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
+
+    # Point pdfkit configuration to wkhtmltopdf.exe
+    config = pdfkit.configuration(wkhtmltopdf=path_to_wkhtmltopdf)
+
+    output_path = tempfile.mktemp(".pdf")
+    # Convert HTML file to PDF
+    pdfkit.from_file(html_file, output_path=output_path, configuration=config)
+
+    return output_path
+
+
+# 开始打印方法
+def start_print():
+    # result_text = tiMuEntity.get("1.0", END)
+    print("打印内容：" + print_html[0])
+    printer = printerEntity.get()
+    print("打印机：" + printer)
+    filename = tempfile.mktemp(".html")
+    open(filename, "w", encoding="UTF-8").write(print_html[0])
+    print(filename)
+    pdf_name = html_to_pdf(filename)
+    print(pdf_name)
+
+    win32api.ShellExecute(0, "print", '"%s"' % pdf_name, '"%s"' % printer, ".", 0)
+
+
+# 出题方法
+def chu_ti():
+    print("一共有几道题: {}".format(countValue.get()))
+    print("运算类型: {}".format(optValue.get()))
+    print("几个数运算: {}".format(optCountValue.get()))
+    print("计算结果最大值: {}".format(resultValue.get()))
+
+    # 清空所有数据
+    tis = question_bank.chu_ti(100, resultValue.get(), optCountValue.get(), countValue.get())
+    html = html_template.shi_juan(tis, 3, 14)
+    print_html.append(html)
+    tiMuEntity.set_html(html)
+
+
+# 下载PDF文件
+def download_pdf():
+    filename = tempfile.mktemp(".html")
+    open(filename, "w", encoding="UTF-8").write(print_html[0])
+    pdf_name = html_to_pdf(filename)
+    print(pdf_name)
+    subprocess.Popen([pdf_name], shell=True)
+
+
+Button(root, text="出题", command=chu_ti).place(x=10, y=90)
+
+Button(root, text="打印", command=start_print).place(x=100, y=90)
+
+Button(root, text="下载PDF", command=download_pdf).place(x=200, y=90)
+
+# 总题数
+countLab = Label(root, text="一共有几道题")
+countValue = IntVar()
+countEntity = Entry(root, width=6, textvariable=countValue)
+countValue.set(50)
+countLab.place(x=50, y=50, width=100, anchor='nw')
+countEntity.place(x=150, y=50, width=80, anchor='nw')
+
+# 运算类型
+optLab = Label(root, text="运算类型")
+optValue = StringVar()
+optEntity = ttk.Combobox(root, textvariable=optValue)
+optEntity['values'] = ["加法", "减法", "加减混合"]
+optValue.set("加减混合")
+optLab.place(x=230, y=50, width=100, anchor='nw')
+optEntity.place(x=330, y=50, width=80, anchor='nw')
+
+# 几个数运算
+optCountLab = Label(root, text="几个数运算")
+optCountValue = IntVar()
+optCountEntity = Entry(root, textvariable=optCountValue)
+optCountValue.set(3)
+optCountLab.place(x=410, y=50, width=100, anchor='nw')
+optCountEntity.place(x=510, y=50, width=80, anchor='nw')
+
+# 运算结果最大值
+resultLab = Label(root, text="计算结果最大值")
+resultValue = IntVar()
+resultEntity = Entry(root, textvariable=resultValue)
+resultValue.set(200)
+resultLab.place(x=590, y=50, width=100, anchor='nw')
+resultEntity.place(x=690, y=50, width=80, anchor='nw')
+
+# 出题结果
+tiMuEntity = HTMLText(root)
+tiMuEntity.place(x=10, y=130, width=max_width - 20, height=870)
+
+root.mainloop()
